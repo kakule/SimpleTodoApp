473a7b9f259191584e55cc60028ac07022ac317e
Phoenix <sinkari.kassim@gmail.com>
2016-09-17 00:57:02 -0700

    Initial commit. First working version of SimpleTodo App

---
+++ app/src/main/java/com/codepath/simpletodoapp/EditItemActivity.java
@@ -0,0 +1,39 @@
+package com.codepath.simpletodoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class EditItemActivity extends AppCompatActivity {
+    int changeIndex = -1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+        changeIndex = getIntent().getIntExtra("position", -1);
+        String initText = getIntent().getStringExtra("field_text");
+        int curPos = initText.length();
+
+        if (initText == null) {
+            initText = "";
+        }
+        EditText etEditItem = (EditText) findViewById(R.id.etEditItem);
+        etEditItem.setText(initText);
+        etEditItem.setSelection(curPos);
+    }
+
+    public void onAddItem(View v) {
+        EditText etEditItem = (EditText) findViewById(R.id.etEditItem);
+        String itemText = etEditItem.getText().toString();
+        if (!itemText.trim().isEmpty() && changeIndex >= 0) {
+            Intent d = new Intent();
+            d.putExtra("field_text", itemText.trim());
+            d.putExtra("position", changeIndex);
+            setResult(RESULT_OK, d);
+        }
+        finish();
+    }
+
+}
